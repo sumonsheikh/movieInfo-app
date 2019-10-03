17dce6b52f4937cd468d6961744ad2142ea3271d
flow <flow@Kamruls-Mac-mini.local>
2019-10-03 12:22:55 +0600

    movie list get successfully

--- js/main.js
+++ js/main.js
@@ -13,6 +13,21 @@ function getMovies(searchText){
     axios.get('http://www.omdbapi.com/?apikey=6e66f068&s='+searchText)
     .then((res)=>{
         console.log(res);
+        let movies=res.data.Search;
+        let output='';
+        $.each(movies,(index,movie)=>{
+            output +=`
+            <div class="col-md-3">
+                <div class="well text-center">
+                    <img src="${movie.Poster}"/>
+                    <h4>${movie.Title}</h4>
+                    <a  onClick="movieSelected('${movie.imdbID}')" href="movie.html" class="btn btn-primary">Movie Details</a>
+                </div>
+            </div>
+            
+            `;
+        });
+        $('#movies').html(output);
     })
     .catch((err)=>{
         console.log(err);
